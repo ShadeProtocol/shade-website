46ce9907f8826861bf6c8c6502a89e8471cad4cb
HollowByt3 <abdul255@tutanota.com>
2025-05-12 03:21:13 -0500

    Build top section

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import FAQSection from '@/components/sections/faq';
 import Footer from '@/components/sections/footer';
 import HowItWorks  from '@/components/sections/howItWorks';
 import ShadeDevelopers  from '@/components/sections/shadeDevelopers';
+import HeroSection from '@/components/sections/HeroSection';
 
 export default function Home() {
   return (
     <main className="flex flex-col row-start-2 items-center sm:items-start">
+      <HeroSection />
       <HowItWorks />
       <ShadeDevelopers />
       <FAQSection />
